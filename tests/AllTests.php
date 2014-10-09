be475a25606a1a8311a0b01367b52563100af8be
Edson Medina <emedina@datix.co.uk>
2014-10-09 15:59:04 +0100

    Adding the actual test files would help

---
+++ tests/AllTests.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+class AllTests
+{
+    public static function suite()
+    {
+        $suite = new PHPUnit_Framework_TestSuite('php_testability unit tests');
+
+        self::addDir(__DIR__, $suite);
+
+        return $suite;
+    }
+
+	private static function addDir ($path, $suite)
+    {
+        $dir = new RecursiveIteratorIterator (new RecursiveDirectoryIterator(realpath($path)));
+        foreach ($dir as $file)
+        {
+            // skip dirs
+            if ($file->isDir()) {
+                continue;
+            }
+
+            // skip non-tests
+            if (substr($file, -8) == 'Test.php')
+            {
+                // remove extension
+                $className = substr($file->getFilename(), 0, -4);
+
+                require_once $file;
+                $suite->addTestSuite($className);
+            }
+        }
+    }
+}
---
+++ tests/FileIteratorTest.php
@@ -0,0 +1,38 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+use edsonmedina\php_testability\FileIterator;
+
+class FileIteratorTest extends PHPUnit_Framework_TestCase
+{
+	public function testIsDirExcluded ()
+	{
+		$analyser = $this->getMockBuilder('edsonmedina\php_testability\Analyser')->disableOriginalConstructor()->getMock();
+
+		$fileIterator = new FileIterator ('', $analyser);
+		$fileIterator->setExcludedDirs (array('whatever/thirdparty', 'other'));
+
+		$this->assertTrue ($fileIterator->isDirExcluded('bla/whatever/thirdparty'));
+		$this->assertTrue ($fileIterator->isDirExcluded('bla/whatever/other'));
+		$this->assertFalse ($fileIterator->isDirExcluded('bla/whatever/somedir'));
+		$this->assertFalse ($fileIterator->isDirExcluded('bla/whatever/tests'));
+		$this->assertFalse ($fileIterator->isDirExcluded('bla/whatever/.git'));
+		$this->assertFalse ($fileIterator->isDirExcluded('bla/whatever/vendor'));
+		$this->assertFalse ($fileIterator->isDirExcluded('bla/whatever/tmp'));
+		$this->assertFalse ($fileIterator->isDirExcluded('bla/whatever/temp'));
+	}	
+
+	public function testIsDirExcludedDefaultValues ()
+	{
+		$analyser = $this->getMockBuilder('edsonmedina\php_testability\Analyser')->disableOriginalConstructor()->getMock();
+
+		$fileIterator = new FileIterator ('', $analyser);
+
+		$this->assertFalse ($fileIterator->isDirExcluded('bla/whatever/somedir'));
+		$this->assertTrue ($fileIterator->isDirExcluded('bla/whatever/tests'));
+		$this->assertTrue ($fileIterator->isDirExcluded('bla/whatever/.git'));
+		$this->assertTrue ($fileIterator->isDirExcluded('bla/whatever/vendor'));
+		$this->assertTrue ($fileIterator->isDirExcluded('bla/whatever/tmp'));
+		$this->assertTrue ($fileIterator->isDirExcluded('bla/whatever/temp'));
+	}	
+}
