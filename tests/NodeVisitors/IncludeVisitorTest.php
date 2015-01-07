4b8b156dd43c986d32b19ec55936fd106930276e
Edson Medina <edsonmedina@gmail.com>
2015-01-07 03:08:56 +0000

    New unit test

---
+++ tests/NodeVisitors/IncludeVisitorTest.php
@@ -0,0 +1,121 @@
+<?php
+
+require_once __DIR__.'/../../vendor/autoload.php';
+use edsonmedina\php_testability\NodeVisitors\IncludeVisitor;
+use edsonmedina\php_testability\TraverserFactory;
+use PhpParser\Node\Expr\Include_;
+use PhpParser\Node\Expr\Variable;
+
+class IncludeVisitorTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\IncludeVisitor::leaveNode
+	 */
+	public function testleaveNodeWithDifferentType ()
+	{
+		$data = $this->getMockBuilder('edsonmedina\php_testability\ReportData')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$scope = $this->getMockBuilder('edsonmedina\php_testability\AnalyserScope')
+		              ->disableOriginalConstructor()
+		              ->getMock();
+		              
+		$factory = $this->getMock ('edsonmedina\php_testability\TraverserFactory');
+
+		$node = $this->getMockBuilder ('PhpParser\Node\Expr\Exit_')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$visitor = new IncludeVisitor ($data, $scope, $factory);
+		$visitor->leaveNode ($node);
+	}	
+
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\IncludeVisitor::leaveNode
+	 */
+	public function testleaveNodeInGlobalSpace ()
+	{
+		$data = $this->getMockBuilder('edsonmedina\php_testability\ReportData')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$scope = $this->getMockBuilder('edsonmedina\php_testability\AnalyserScope')
+		              ->disableOriginalConstructor()
+		              ->getMock();
+
+		$scope->method ('inGlobalSpace')->willReturn (true);
+
+		$factory = $this->getMock ('edsonmedina\php_testability\TraverserFactory');
+
+		$node = $this->getMockBuilder ('PhpParser\Node\Expr\Include_')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$visitor = new IncludeVisitor ($data, $scope, $factory);
+		$visitor->leaveNode ($node);
+	}	
+
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\IncludeVisitor::leaveNode
+	 */
+	public function testleaveNodeWithAutoloader ()
+	{
+		$data = $this->getMockBuilder('edsonmedina\php_testability\ReportData')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$scope = $this->getMockBuilder('edsonmedina\php_testability\AnalyserScope')
+		              ->disableOriginalConstructor()
+		              ->getMock();
+
+		$scope->method ('inGlobalSpace')->willReturn (false);
+		$scope->method ('getScopeName')->willReturn ('__autoload');
+
+		$factory = $this->getMockBuilder ('edsonmedina\php_testability\TraverserFactory')
+		                ->getMock();
+
+		$node = $this->getMockBuilder ('PhpParser\Node\Expr\Include_')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$visitor = new IncludeVisitor ($data, $scope, $factory);
+		$visitor->leaveNode ($node);
+	}	
+
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\IncludeVisitor::leaveNode
+	 */
+	public function testleaveNode ()
+	{
+		$data = $this->getMockBuilder('edsonmedina\php_testability\ReportData')
+		             ->disableOriginalConstructor()
+		             ->setMethods(array('addIssue'))
+		             ->getMock();
+
+		$data->expects($this->once())
+		     ->method('addIssue')
+		     ->with(
+		           $this->equalTo(7),
+		           $this->equalTo('include'),
+		           $this->equalTo('someScopeName'),
+		           $this->anything()
+		       );
+
+		$scope = $this->getMockBuilder('edsonmedina\php_testability\AnalyserScope')
+		              ->disableOriginalConstructor()
+		              ->getMock();
+
+		$scope->method ('inGlobalSpace')->willReturn (false);
+		$scope->method ('getScopeName')->willReturn ('someScopeName');
+
+		$node = new Include_ (
+			new Variable('test'), 
+			PhpParser\Node\Expr\Include_::TYPE_REQUIRE, 
+			array ('startLine' => 7)
+		);
+
+		$visitor = new IncludeVisitor ($data, $scope, new TraverserFactory);
+		$visitor->leaveNode ($node);
+	}	
+}
