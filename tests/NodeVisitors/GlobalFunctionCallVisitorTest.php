7e9adbb4c49989befa2732fa763cb43e0603c828
Edson Medina <edsonmedina@gmail.com>
2015-01-12 23:50:25 +0000

    More tests

---
+++ tests/NodeVisitors/GlobalFunctionCallVisitorTest.php
@@ -0,0 +1,158 @@
+<?php
+
+require_once __DIR__.'/../../vendor/autoload.php';
+use edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor;
+
+class GlobalFunctionCallVisitorTest extends PHPUnit_Framework_TestCase
+{
+	public function setup ()
+	{
+		$this->data = $this->getMockBuilder('edsonmedina\php_testability\ReportData')
+		                   ->disableOriginalConstructor()
+		                   ->setMethods(array('addIssue'))
+		                   ->getMock();	
+
+		$this->scope = $this->getMockBuilder('edsonmedina\php_testability\AnalyserScope')
+		                    ->disableOriginalConstructor()
+		                    ->getMock();	
+
+		$this->wrongNode = $this->getMockBuilder ('PhpParser\Node\Expr\StaticCall')
+		                        ->disableOriginalConstructor()
+		                        ->getMock();
+
+		$this->factory = $this->getMockBuilder ('edsonmedina\php_testability\TraverserFactory')
+		                      ->setMethods(array('getDictionary','getNodeWrapper'))
+		                      ->getMock();
+	}
+
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor::__construct
+	 * @covers edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor::leaveNode
+	 */
+	public function testLeaveNodeWithDifferentType ()
+	{
+		$this->factory->method ('getDictionary')->willReturn (false);
+
+		// data
+		$this->data->expects($this->never())
+		     ->method('addIssue');
+		     
+		$visitor = new GlobalFunctionCallVisitor ($this->data, $this->scope, $this->factory);
+		$visitor->leaveNode ($this->wrongNode);
+	}
+
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor::__construct
+	 * @covers edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor::leaveNode
+	 */
+	public function testLeaveNodeInGlobalSpace ()
+	{
+		$this->factory->method ('getDictionary')->willReturn (false);
+
+		$this->scope->method ('inGlobalSpace')->willReturn (true);
+
+		$node = $this->getMockBuilder ('PhpParser\Node\Expr\FuncCall')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		// data
+		$this->data->expects($this->never())
+		     ->method('addIssue');
+
+		$visitor = new GlobalFunctionCallVisitor ($this->data, $this->scope, $this->factory);
+		$visitor->leaveNode ($this->wrongNode);
+	}
+
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor::__construct
+	 * @covers edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor::leaveNode
+	 */
+	public function testLeaveNodeWithInternalFunction ()
+	{
+		// dictionary
+		$dictionary = $this->getMockBuilder ('edsonmedina\php_testability\Dictionary')
+		                   ->setMethods(array('isInternalFunction'))
+		                   ->getMock();
+
+		$dictionary->method ('isInternalFunction')->willReturn (true);
+
+		// factory
+		$this->factory->method ('getDictionary')->willReturn ($dictionary);
+
+		// scope
+		$this->scope->method ('inGlobalSpace')->willReturn (false);
+
+		// node
+		$node = $this->getMockBuilder ('PhpParser\Node\Expr\FuncCall')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+        // node wrapper
+		$nodewrapper = $this->getMockBuilder ('edsonmedina\php_testability\NodeWrapper')
+		                    ->disableOriginalConstructor()
+		                    ->getMock();
+
+		$nodewrapper->method ('getName')->willReturn ('Test::foo');
+
+		// data
+		$this->data->expects($this->never())
+		     ->method('addIssue');
+
+		// factory
+		$this->factory->method ('getNodeWrapper')->willReturn ($nodewrapper);
+
+		$visitor = new GlobalFunctionCallVisitor ($this->data, $this->scope, $this->factory);
+		$visitor->leaveNode ($node);
+	}
+
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor::__construct
+	 * @covers edsonmedina\php_testability\NodeVisitors\GlobalFunctionCallVisitor::leaveNode
+	 */
+	public function testLeaveNode ()
+	{
+		// dictionary
+		$dictionary = $this->getMockBuilder ('edsonmedina\php_testability\Dictionary')
+		                   ->setMethods(array('isInternalFunction'))
+		                   ->getMock();
+
+		$dictionary->method ('isInternalFunction')->willReturn (false);
+
+		// factory
+		$this->factory->method ('getDictionary')->willReturn ($dictionary);
+
+		// scope
+		$this->scope->method ('inGlobalSpace')->willReturn (false);
+		$this->scope->method ('getScopeName')->willReturn ('someScopeName');
+
+		// node
+		$node = $this->getMockBuilder ('PhpParser\Node\Expr\FuncCall')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$node->method ('getLine')->willReturn (7);
+
+        // node wrapper
+		$nodewrapper = $this->getMockBuilder ('edsonmedina\php_testability\NodeWrapper')
+		                    ->disableOriginalConstructor()
+		                    ->getMock();
+
+		$nodewrapper->method ('getName')->willReturn ('Test::foo');
+
+		// factory
+		$this->factory->method ('getNodeWrapper')->willReturn ($nodewrapper);
+
+		// data
+		$this->data->expects($this->once())
+		     ->method('addIssue')
+		     ->with(
+		           $this->equalTo(7),
+		           $this->equalTo('global_function_call'),
+		           $this->equalTo('someScopeName'),
+		           $this->equalTo('Test::foo')
+		       );
+
+		$visitor = new GlobalFunctionCallVisitor ($this->data, $this->scope, $this->factory);
+		$visitor->leaveNode ($node);
+	}
+}
