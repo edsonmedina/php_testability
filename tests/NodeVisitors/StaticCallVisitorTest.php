684fb6aa39a152dcba6cdf07510b8e4105224871
Edson Medina <edsonmedina@gmail.com>
2015-01-23 17:47:21 +0000

    Fixed unit test

--- tests/NodeVisitors/StaticCallVisitorTest.php
+++ tests/NodeVisitors/StaticCallVisitorTest.php
@@ -59,6 +59,60 @@ class StaticCallVisitorTest extends PHPUnit_Framework_TestCase
 	 * @covers edsonmedina\php_testability\NodeVisitors\StaticCallVisitor::__construct
 	 * @covers edsonmedina\php_testability\NodeVisitors\StaticCallVisitor::leaveNode
 	 */
+	public function testLeaveNodeWithSafeClass ()
+	{
+		// data
+		$data = $this->getMockBuilder('edsonmedina\php_testability\ReportData')
+		             ->disableOriginalConstructor()
+		             ->setMethods(array('addIssue'))
+		             ->getMock();
+
+		$data->expects($this->never())->method('addIssue');
+
+		// scope
+		$scope = $this->getMockBuilder('edsonmedina\php_testability\AnalyserScope')
+		              ->disableOriginalConstructor()
+		              ->getMock();
+
+		$scope->method ('inGlobalSpace')->willReturn (false);
+		$scope->method ('getScopeName')->willReturn ('someScopeName');
+
+        // node wrapper
+		$nodewrapper = $this->getMockBuilder ('edsonmedina\php_testability\NodeWrapper')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$nodewrapper->method ('getName')->willReturn ('foo');
+
+		// dictionary
+		$dictionary = $this->getMockBuilder ('edsonmedina\php_testability\Dictionary')
+		                   ->disableOriginalConstructor()
+		                   ->getMock();
+
+		$dictionary->method ('isClassSafeForInstantiation')->willReturn (true);
+
+		// factory
+		$factory = $this->getMockBuilder ('edsonmedina\php_testability\TraverserFactory')
+		                ->getMock();
+
+		$factory->method ('getNodeWrapper')->willReturn ($nodewrapper);
+		$factory->method ('getDictionary')->willReturn ($dictionary);
+
+		// node
+		$node = $this->getMockBuilder ('PhpParser\Node\Expr\StaticCall')
+		             ->disableOriginalConstructor()
+		             ->getMock();
+
+		$node->method ('getLine')->willReturn (7);
+
+		$visitor = new StaticCallVisitor ($data, $scope, $factory);
+		$visitor->leaveNode ($node);
+	}	
+
+	/**
+	 * @covers edsonmedina\php_testability\NodeVisitors\StaticCallVisitor::__construct
+	 * @covers edsonmedina\php_testability\NodeVisitors\StaticCallVisitor::leaveNode
+	 */
 	public function testLeaveNode ()
 	{
 		// data
@@ -91,11 +145,19 @@ class StaticCallVisitorTest extends PHPUnit_Framework_TestCase
 
 		$nodewrapper->method ('getName')->willReturn ('foo');
 
+		// dictionary
+		$dictionary = $this->getMockBuilder ('edsonmedina\php_testability\Dictionary')
+		                   ->disableOriginalConstructor()
+		                   ->getMock();
+
+		$dictionary->method ('isClassSafeForInstantiation')->willReturn (false);
+
 		// factory
 		$factory = $this->getMockBuilder ('edsonmedina\php_testability\TraverserFactory')
 		                ->getMock();
 
 		$factory->method ('getNodeWrapper')->willReturn ($nodewrapper);
+		$factory->method ('getDictionary')->willReturn ($dictionary);
 
 		// node
 		$node = $this->getMockBuilder ('PhpParser\Node\Expr\StaticCall')
