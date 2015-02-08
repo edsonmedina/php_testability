ad42166dd3516d416e1f345cdd7df17ce6572e12
Edson Medina <edsonmedina@gmail.com>
2015-02-08 19:01:52 +0000

    New unit test (#63)

---
+++ tests/Issues/CodeOnGlobalSpaceIssueTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use edsonmedina\php_testability\Issues\CodeOnGlobalSpaceIssue;
+
+class CodeOnGlobalSpaceIssueTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @covers edsonmedina\php_testability\Issues\CodeOnGlobalSpaceIssue::getTitle
+	 */
+	public function testGetTitle ()
+	{
+		$node = $this->getMockBuilder('PhpParser\Node\Stmt\Class_')
+		              ->disableOriginalConstructor()
+		              ->getMock();
+
+		$issue = new CodeOnGlobalSpaceIssue ($node);
+
+		$this->assertEquals('Code on global space', $issue->getTitle());
+	}
+}
