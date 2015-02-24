d730c50d79c80224e09832e6f5733d7cc705487c
Edson Medina <edsonmedina@gmail.com>
2015-02-24 01:21:20 +0000

    New unit test

---
+++ tests/Contexts/DirectorySpecificationTest.php
@@ -0,0 +1,32 @@
+<?php
+
+require_once __DIR__.'/../../vendor/autoload.php';
+
+use edsonmedina\php_testability\Contexts\DirectorySpecification;
+use edsonmedina\php_testability\Contexts\DirectoryContext;
+use edsonmedina\php_testability\Contexts\FileContext;
+use edsonmedina\php_testability\Contexts\ClassContext;
+
+class DirectorySpecificationTest extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * @covers edsonmedina\php_testability\Contexts\DirectorySpecification::isSatisfiedBy
+	 */
+	public function testIsSatisfiedByInvalid ()
+	{
+		$spec = new DirectorySpecification ();
+
+		$this->assertFalse($spec->isSatisfiedBy(new FileContext('zzz')));
+		$this->assertFalse($spec->isSatisfiedBy(new ClassContext('zzz', 1, 2)));
+	}
+
+	/**
+	 * @covers edsonmedina\php_testability\Contexts\DirectorySpecification::isSatisfiedBy
+	 */
+	public function testIsSatisfiedBy ()
+	{
+		$spec = new DirectorySpecification ();
+
+		$this->assertTrue($spec->isSatisfiedBy(new DirectoryContext('zzz')));
+	}
+}
